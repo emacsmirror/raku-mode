62da394dae0ed3e34ee6b262d9d76e18aa888a65
Zak B. Elep <zakame@zakame.net>
2020-04-22 16:24:56 +0800

    Update for transition to raku-mode
    - Let cask generate a `perl6-mode-pkg.el` to depend on `raku-mode`
    - Let `perl6-mode.el` just set a defalias to `raku-mode`.

--- Cask
+++ Cask
@@ -3,7 +3,4 @@
 
 (package-file "perl6-mode.el")
 
-(files "*.el")
-
-(development
- (depends-on "ert-runner"))
+(depends-on "raku-mode")
--- perl6-mode.el
+++ perl6-mode.el
@@ -27,68 +27,11 @@
 
 ;; GNU Emacs 24 major mode for editing Perl 6 code.
 
-;; Currently only provides very basic syntax highlighting.
+;; This is now a transitory mode/package for `raku-mode'.
 
 ;;; Code:
 
-(declare-function pkg-info-version-info "pkg-info" (library))
-
-(defgroup perl6 nil
-  "Major mode for editing Perl 6 code."
-  :prefix "perl6-"
-  :group 'language)
-
-(require 'perl6-detect)
-(require 'perl6-font-lock)
-(require 'perl6-indent)
-(require 'perl6-imenu)
-(require 'perl6-repl)
-
-(defvar perl6-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-c") 'perl6-send-line-to-repl)
-    (define-key map (kbd "C-c C-r") 'perl6-send-region-to-repl)
-    (define-key map (kbd "C-c C-h") 'perl6-send-buffer-to-repl)
-    map)
-  "Keymap for `perl6-mode'.")
-
-(easy-menu-define perl6-mode-menu perl6-mode-map
-  "Menu for `perl6-mode'"
-  '("Raku"
-    ["Send line to repl" perl6-send-line-to-repl]
-    ["Send region to repl" perl6-send-region-to-repl]
-    ["Send buffer to repl" perl6-send-buffer-to-repl]))
-
-;;;###autoload
-(define-derived-mode perl6-mode prog-mode "Perl6"
-  "Major mode for editing Perl 6 code."
-  ;; Syntaxification and font locking
-  (setq-local syntax-propertize-function #'perl6-syntax-propertize)
-  (add-hook 'syntax-propertize-extend-region-functions #'syntax-propertize-multiline nil 'local)
-  (setq-local font-lock-syntactic-face-function #'perl6-font-lock-syntactic-face)
-  (setq-local font-lock-defaults '(perl6-font-lock-keywords nil nil))
-  ;; Add imenu support for perl6-mode.  Note that imenu-generic-expression
-  ;; is buffer-local, so we don't need a local-variable for it.
-  (add-hook 'perl6-mode-hook 'imenu-add-menubar-index)
-  (setq imenu-generic-expression perl6-imenu-generic-expression
-      imenu-case-fold-search nil)
-  ;; Comments
-  (setq-local comment-start "#")
-  (setq-local comment-start-skip "#+ *")
-  (setq-local comment-use-syntax t)
-  (setq-local comment-end "")
-   ;; REPL
-  (setq comint-prompt-regexp perl6-prompt-regexp)
-  (setq comint-prompt-read-only t)
-  (set (make-local-variable 'paragraph-start) perl6-prompt-regexp)
-  ;; Indentation (see SMIE in the Emacs manual)
-  ;; TODO add rules for HEREDOC indentation
-  (smie-setup perl6-smie-grammar #'perl6-smie-rules
-              :forward-token #'perl6-smie--forward-token
-              :backward-token #'perl6-smie--backward-token)
-  (use-local-map perl6-mode-map))
-
-
+(defalias 'perl6-mode 'raku-mode)
 
 (provide 'perl6-mode)
 
